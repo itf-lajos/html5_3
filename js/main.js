dbc3f966d50a187ee6410f9e2b3891ea3c55fef0
itf-lajos <itf.lajos@gmail.com>
2015-09-28 10:57:33 +0200

    második videó vége

--- js/main.js
+++ js/main.js
@@ -1,9 +1,19 @@
 // Elemek kiválasztása
 // @param selector: string az elemet kiválasztó szelektor
 function _q( selector ){
-
     // Elemek kiválasztása.
     var elements = document.querySelectorAll( selector );
-    
     return elements;
+}
+
+// Profil beviteli mező megjelenítése
+function showProfile() {
+    // Elemek kiválasztása.
+    var div = document.querySelector( ".profile-div" );
+    // console.log( div );
+    if (div.classList.contains( "show" ) ) {
+        div.classList.remove( "show" );
+    } else {
+        div.classList.add( "show" );
+    }
 }
